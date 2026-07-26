fc585c0d749da550b6cd8ee98d5ab2c5200cbc54
bridmanani <brimanani21@gmail.com>
2026-07-26 00:20:13 -0500

    Update EDA.R

--- scripts/EDA.R
+++ scripts/EDA.R
@@ -1,1 +1,14 @@
 # Análisis Exploratorio de Datos - ENAHO
+# Proyecto Final - Ofimatica
+## Descripción
+
+Este proyecto presenta un análisis exploratorio de datos (EDA) utilizando la base ENAHO.
+
+## Contenido
+
+- data/: base de datos utilizada.
+- scripts/: código en R.
+- figures/: gráficos obtenidos durante el análisis.
+
+## Autor
+- BRIDGIT OLGA MANANÍ CARRIÓN
